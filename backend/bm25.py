48413f3fa94ded77e53f33b5a5a23bfaac792bfb
Yang Jiang <jyang96069@gmail.com>
2022-04-25 17:57:45 -0500

    initial implementation of backend

--- backend/bm25.py
+++ backend/bm25.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from rank_bm25 import BM25Okapi
 from secret import API_KEY
@@ -35,6 +36,21 @@ def get_corpus(records):
     return corpus
 
 
+def get_top_k(scores, records, k):
+    if (k > len(scores)):
+        k = len(scores)
+
+    scores_with_idx = [(scores[i], i) for i in range(len(scores))]
+    sorted_with_idx = sorted(
+        scores_with_idx, key=lambda item: item[0], reverse=True)
+
+    res = []
+    for i in range(k):
+        res.append(records[sorted_with_idx[i][1]])
+
+    return res
+
+
 def rank(text, size):
     """
     Fetch top @code{size} docs related to the given @code{text}
@@ -49,21 +65,31 @@ def rank(text, size):
             - url: url of the article
     """
     API_RETURN_SIZE = 10
+    RE_RULE = '-| '
     URL_TEMPLETE = "https://api.springernature.com/metadata/json?q=keyword:{}&p={}&api_key={}"
 
     response = requests.get(URL_TEMPLETE.format(
         text, API_RETURN_SIZE, API_KEY))
-
     records = parse_records(response.json()['records'])
 
-    return records
+    corpus = [re.split(RE_RULE, doc.lower()) for doc in get_corpus(records)]
+    bm25 = BM25Okapi(corpus)
+    scores = bm25.get_scores(re.split(RE_RULE, text.lower()))
+
+    return get_top_k(scores, records, size)
 
 
 if __name__ == "__main__":
     # an example of springer API useage
     url_templete = "https://api.springernature.com/metadata/json?q=keyword:{}&p={}&api_key={}"
     size = 10
-    query = "covid"
+    query = "covid 19"
     response = requests.get(url_templete.format(query, size, API_KEY))
     records = parse_records(response.json()['records'])
-    corpus = get_corpus(records)
+    split_str = '-| '
+    corpus = [re.split(split_str, doc.lower()) for doc in get_corpus(records)]
+
+    bm25 = BM25Okapi(corpus)
+    scores = bm25.get_scores(re.split(split_str, query.lower()))
+    print(scores)
+    print(get_top_k(scores, records, 10000))
